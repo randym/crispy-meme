4aec8734be42cffa416a3c06bef050e35180fe89
Randy Morgan <digital.ipseity@gmail.com>
2024-11-17 23:54:07 +0900

    feat(infinite_scrolling_list): enhance component with click handling and Material Tailwind styling

--- app/javascript/src/components/infinite_scrolling_list.tsx
+++ app/javascript/src/components/infinite_scrolling_list.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { useScrollingPagination } from "../hooks";
-
+import { List } from "@material-tailwind/react";
 interface InfiniteScrollingListProps<T extends Identifiable> {
   source: Paginating<T>;
   firstPage: readonly T[];
   ComponentType: React.ComponentType<{ item: T }>;
+  onItemClick?: (item: T) => void;
 }
 
 type InfiniteScrollingList = React.FC<InfiniteScrollingListProps<any>>;
@@ -16,11 +17,16 @@ export const InfiniteScrollingList: InfiniteScrollingList = (props) => {
   const { list, isLoading } = useScrollingPagination(source, firstPage);
 
   return (
-    <div>
+    <List className="w-full">
       {list.map((item: any) => (
-        <ComponentType item={item} key={item.id} />
+        <div
+          key={item.id}
+          onClick={() => props.onItemClick && props.onItemClick(item)}
+        >
+          <ComponentType item={item} key={item.id} />
+        </div>
       ))}
       {isLoading && <div className="text-center">Loading...</div>}
-    </div>
+    </List>
   );
 };
