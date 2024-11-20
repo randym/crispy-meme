adef701f5a4feed74c40415a3f0691ada3f95ff7
Randy Morgan <digital.ipseity@gmail.com>
2024-11-21 00:06:18 +0900

    chore(cleanup): extract types to types/

--- app/javascript/src/models/city.tsx
+++ app/javascript/src/models/city.tsx
@@ -1,38 +1,6 @@
 import { City as ViewModel } from "../view_models";
 import { Cities as Remote } from "../lib/api";
 
-type JsonApiCity = JsonApiResourceIdentifier & {
-  attributes: {
-    name: string;
-  };
-  relationships: {
-    households: {
-      data: { id: number; type: string }[];
-    };
-    energyProductions: {
-      data: { id: number; type: string }[];
-    };
-  };
-};
-
-type JsonApiCitiesDocument = {
-  data: JsonApiCity[];
-  included: JsonApiHousehold[] | JsonApiEnergyProduction[];
-};
-
-type CityTransformer = {
-  transform: (json: JsonApiCitiesDocument) => CityModel | CityModel[];
-  parseJsonApi: ({
-    json,
-    jsonApiHouseholds,
-    jsonApiEnergyProductions,
-  }: {
-    json: JsonApiCity;
-    jsonApiHouseholds: JsonApiHousehold[];
-    jsonApiEnergyProductions: JsonApiEnergyProduction[];
-  }) => CityModel;
-};
-
 export class City implements CityModel {
   id: number = 0;
   name: string = "";
